4968a71e28d1409c00a269115d7ab11416ba708c
An Doan <minh.an.doan@gmail.com>
2017-12-04 21:36:16 -0800

    Implement boggle board solver.

---
+++ week4/boggle/src/BoggleSolver.java
@@ -0,0 +1,121 @@
+import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
+
+public class BoggleSolver
+{
+    private TST<Boolean> dict;
+    SeparateChainingHashST<Integer, Queue<Integer>> adj;
+
+    // Initializes the data structure using the given array of strings as the dictionary.
+    // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
+    public BoggleSolver(String[] dictionary)
+    {
+        dict = new TST<>();
+        for(int i = 0; i < dictionary.length; i++)
+        {
+            dict.put(dictionary[i], true);
+        }
+    }
+
+    // Returns the set of all valid words in the given Boggle board, as an Iterable.
+    public Iterable<String> getAllValidWords(BoggleBoard board)
+    {
+        Queue<String> validWords = new Queue<>();
+    }
+
+    private void adjacentBlocks(BoggleBoard board)
+    {
+        adj = new SeparateChainingHashST<>();
+        for(int i = 0; i < board.rows(); i++)
+        {
+            for(int j = 0; j < board.cols(); j++)
+            {
+                Queue<Integer> adjToIJ = new Queue<>();
+                if(i != 0)
+                {
+                    adjToIJ.enqueue((i-1)*board.cols() + j);
+                    if(j != 0)
+                    {
+                        adjToIJ.enqueue((i-1)*board.cols() + j - 1);
+                    }
+                    if(j != board.cols())
+                    {
+                        adjToIJ.enqueue((i-1)*board.cols() + j + 1);
+                    }
+                }
+                if(i != board.rows()-1)
+                {
+                    adjToIJ.enqueue((i+1)*board.cols() + j);
+                    if(j != 0)
+                    {
+                        adjToIJ.enqueue((i+1)*board.cols() + j - 1);
+                    }
+                    if(j != board.cols())
+                    {
+                        adjToIJ.enqueue((i+1)*board.cols() + j + 1);
+                    }
+                }
+                if(j != 0)
+                {
+                    adjToIJ.enqueue((i)*board.cols() + j - 1);
+                }
+                if(j != board.cols())
+                {
+                    adjToIJ.enqueue((i)*board.cols() + j + 1);
+                }
+                adj.put(i*board.cols()+j, adjToIJ);
+            }
+        }
+    }
+
+
+    private void dfs(BoggleBoard board)
+    {
+        int tiles = board.rows() * board.cols();
+        for(int v = 0; v < tiles; v++)
+        {
+            boolean[] marked = new boolean[tiles];
+            StringBuilder prefix = new StringBuilder();
+            dfs(board, v, prefix, marked);
+        }
+    }
+
+    private void dfs(BoggleBoard board, int v, StringBuilder prefix, boolean[] marked)
+    {
+        marked[v] = true;
+        int row = v/board.cols();
+        int col = v%board.cols();
+        prefix.append(board.getLetter(row, col));
+
+        Queue<String>  wordsWithPrefix = (dict.keysWithPrefix(prefix.toString());
+        if(!wordsWithPrefix.isEmpty()) {
+            for (int w : adj.get(v)) {
+                if (!marked[w]) {
+                    dfs(board, w, prefix, marked);
+                }
+            }
+        }
+        marked[v] = false;
+    }
+
+    // Returns the score of the given word if it is in the dictionary, zero otherwise.
+    // (You can assume the word contains only the uppercase letters A through Z.)
+    public int scoreOf(String word)
+    {
+
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        String[] dictionary = in.readAllStrings();
+        BoggleSolver solver = new BoggleSolver(dictionary);
+        BoggleBoard board = new BoggleBoard(args[1]);
+        int score = 0;
+        for (String word : solver.getAllValidWords(board)) {
+            StdOut.println(word);
+            score += solver.scoreOf(word);
+        }
+        StdOut.println("Score = " + score);
+    }
+}
