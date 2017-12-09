14a0629c718d001b0529515d8a1172b174704057
An Doan <minh.an.doan@gmail.com>
2017-12-09 10:33:25 -0800

    Start Burrows-Wheeler data compression implementation.

---
+++ week5/burrows/src/CircularSuffixArray.java
@@ -0,0 +1,27 @@
+public class CircularSuffixArray {
+
+    // circular suffix array of s
+    public CircularSuffixArray(String s)
+    {
+
+    }
+
+    // length of s
+    public int length()
+    {
+        return  0;
+    }
+
+    // returns index of ith sorted suffix
+    public int index(int i)
+    {
+        return 0;
+    }
+
+    // unit testing (required)
+    public static void main(String[] args)
+    {
+
+    }
+
+}
